37b560f48b633df79d63d42ae7d670abca9fe2c3
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-03-21 20:51:02 +0300

    Add files via upload

---
+++ ProductInfo.cs
@@ -0,0 +1,8 @@
+﻿internal class ProductInfo
+{
+    public string Name { get; set; } = "";
+    public int Price { get; set; } = 0;
+    public int NumberlnStock { get; set; } = 0;
+    public override string ToString()
+    => $"Name={Name}, Descnption= {Price}, Number in Stock={NumberlnStock}";
+}
